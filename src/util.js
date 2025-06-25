dcab755dfe6f0a7e7a7cde4e6f7ff0c9b2dda7c3
SandeepKumarNayak09 <sandeepkumarnayak068@gmail.com>
2025-06-25 16:55:48 +0530

    remove explicit backend uri

--- src/util.js
+++ src/util.js
@@ -3,4 +3,4 @@ import {toast} from 'react-toastify';
 export const notify=(message,type)=>{
     toast[type](message);
 }
-export const API_URL = process.env.BACKEND_URI;
+export const API_URL = process.env.REACT_APP_BACKEND_URI;
